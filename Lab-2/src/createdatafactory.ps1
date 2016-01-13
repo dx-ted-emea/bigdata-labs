2e361bb7d73a8276fb2a9f95d75436a222a943bf
nava <navale@microsoft.com>
2016-01-13 16:35:52 +0200

    powershell 1.0.0

--- Lab-2/src/createdatafactory.ps1
+++ Lab-2/src/createdatafactory.ps1
@@ -1,16 +1,13 @@
-
-Switch-AzureMode AzureResourceManager 
-
 # create resource group - uncomment if the resource group was not created yet
-#New-AzureResourceGroup -Name Test_BigData_RG  -Location "North Europe"
+New-AzureRmResourceGroup -Name Test_BigData_RG  -Location "North Europe"
 
 # create data factory - make sure to update the json files with the correct connection string
-New-AzureDataFactory -ResourceGroupName Test_BigData_RG -Name DFBigDatatutorialTest -Location "North Europe"
-$df=Get-AzureDataFactory -ResourceGroupName Test_BigData_RG -Name DFBigDatatutorialTest
+New-AzureRmDataFactory -ResourceGroupName Test_BigData_RG -Name DFBigDatatutorialTest -Location "North Europe"
+$df=Get-AzureRmDataFactory -ResourceGroupName Test_BigData_RG -Name DFBigDatatutorialTest
 
-New-AzureDataFactoryLinkedService $df -File .\StorageLinkedService.json
-New-AzureDataFactoryLinkedService $df -File .\DocumentDbLinkedService.json
-New-AzureDataFactoryDataset $df -File .\BlobTableOut.json
-New-AzureDataFactoryDataset $df -File .\DocumentDbIn.json
-New-AzureDataFactoryPipeline $df -File .\DocDbToBlobPipe.json
+New-AzureRmDataFactoryLinkedService $df -File .\StorageLinkedService.json
+New-AzureRmDataFactoryLinkedService $df -File .\DocumentDbLinkedService.json
+New-AzureRmDataFactoryDataset $df -File .\BlobTableOut.json
+New-AzureRmDataFactoryDataset $df -File .\DocumentDbIn.json
+New-AzureRmDataFactoryPipeline $df -File .\DocDbToBlobPipe.json
 
